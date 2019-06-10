8f4b6696ff12904957dd8e56c33697f5017f9808
H <quanhungbang@gmail.com>
2019-06-10 20:59:15 +0700

    Edit config connect mysql

--- wp-config.php
+++ wp-config.php
@@ -20,18 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp_shoes' );
-
-define( 'DB_PORT', '8889' );
+define( 'DB_NAME', 'heroku_30f118ef131c465' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'bae9073566ada7' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'f53dbc9a' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'us-cdbr-iron-east-02.cleardb.net' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
